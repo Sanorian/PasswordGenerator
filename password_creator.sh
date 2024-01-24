760862c1df4fe5f2c25a60b5f64a9b180a3932d0
Sanorian <krajnov.sascha@yandex.ru>
2024-01-24 15:14:40 +0400

    first commit

---
+++ password_creator.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+chars=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "!" "@" "#" "$" "%" "^" "&" "*" "(" ")" "_" "+" "-" "=" "{" "}" "[" "]" "|" ":" ";" "<" ">" "," "." "?" "/" "~")
+password=""
+for i in $(seq 1 $1)
+do
+    password+=${chars[0 + $RANDOM % (${#chars[@]}-1)]}
+done
+echo $password
